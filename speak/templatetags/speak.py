d3b3cc6c94c93bf2c90ca20f203ba5fbd34290a8
Dana Woodman <dana@danawoodman.com>
2009-11-26 19:02:35 +0100

    Oops, forgot to commit changes to speak.py, doing it now...

--- speak/templatetags/speak.py
+++ speak/templatetags/speak.py
@@ -16,29 +16,62 @@ def pirate(value):
         
     """
     word_dict = {
-        r'\b(you|your)\b': 'yar',
-        r'\b(yours)\b': 'yars',
-        r'\b(hello)\b': 'ahoy',
-        r'\b(mad)\b': 'addled',
-        r'\b(hey)\b': 'arr',
-        r'\b(yes)\b': 'aye',
-        r'\b(stop)\b': 'avast',
-        r'\b(for)\b': 'far',
-        r'\b(treasure)\b': 'booty',
-        r'\b(will come)\b': 'be comin\'',
-        r'\B(ing)\b': 'in\'', # Replaces "ing" endings with "in'"
-        r'\b(in)\b': '\'n',
-        r'\b(lady|woman|female)\b': 'lass',
-        r'\b(friend)\b': 'bucko',
-        r'\b(my)\b': 'me',
-        r'\b(song)\b': 'chanty',
-        r'\b(front)\b': 'fore',
-        r'\b(coin)\b': 'doubloon',
-        r'\b(have a drink|take a drink|get a drink)\b': 'carouse',
-        r'\b(are)\b': 'is',
+        # Adjectives
         r'\b(beautiful|pretty)\b': 'fancy',
-        r'\b(want)\b': 'wants',
+        r'\b(big)\b': 'grand',
+        r'\b(front)\b': 'fore',
+        r'\b(mad)\b': 'addled',
+        r'\b(this)\b': 'tis',
+        # Adverbs
+        r'\b(there)\b': 'thar',
+        r'\b(quickly)\b': 'handsomely',
+        r'\b(yes)\b': 'aye',
+        # Expressions, phrases, and commands
+        r'\b(goodbye|good bye)\b': 'fair winds',
         r'\b(guy|man)\b': 'scallywag',
+        r'\b(haha)\b': 'harhar',
+        r'\b(hello)\b': 'ahoy',
+        r'\b(hey)\b': 'arr',
+        r'\b(stop)\b': 'avast',
+        # Nouns
+        r'\b(alcohol|beer)\b': 'grog',
+        r'\b(boat)\b': 'ship',
+        r'\b(boy|child)\b': 'lad',
+        r'\b(coin)\b': 'doubloon',
+        r'\b(crew|sailors)\b': 'hands',
+        r'\b(eye)\b': 'deadlight',
+        r'\b(eyes)\b': 'deadlights',
+        r'\b(food)\b': 'grub',
+        r'\b(friend)\b': 'bucko',
+        r'\b(girl)\b': 'lassie',
+        r'\b(hell|Hell)\b': 'El',
+        r'\b(lady|woman|female)\b': 'lass',
+        r'\b(money|cash)\b': 'loot',
+        r'\b(prostitute)\b': 'wench',
+        r'\b(rop)\b': 'cord',
+        r'\b(song)\b': 'chanty',
+        r'\b(toilet|bathroom)\b': 'head',
+        r'\b(treasure)\b': 'booty',
+        # Prepositions
+        r'\b(for)\b': 'far',
+        r'\b(in)\b': 'n',
+        r'\b(with)\b': 'wit',
+        # Pronouns
+        r'\b(my)\b': 'me',
+        r'\b(you)\b': 'ye',
+        r'\b(your)\b': 'yar',
+        r'\b(yours)\b': 'yars',
+        # Verbs
+        r'\b(are)\b': 'be',
+        r'\b(have a drink|take a drink|get a drink)\b': 'carouse',
+        r'\b(sleep)\b': 'lay',
+        r'\b(like)\b': 'likes',
+        r'\b(rob)\b': 'pillage',
+        r'\b(steal)\b': 'plunder',
+        r'\b(want)\b': 'wants',
+        r'\b(will)\b': 'be',
+        # Other
+        r'\B(ing)\b': 'in', # Replaces "ing" endings with "in"
     }
     return replacer(value, word_dict)
 
@@ -55,8 +88,24 @@ def shakespeare(value):
 
     """
     word_dict = {
-        r'\b(you)\b': 'thou',
+        # Adjectives
+        
+        # Adverbs
+        
+        # Expressions, phrases, and commands
+        r'\b(it is)\b': 'tis',
+        
+        # Nouns
+        
+        # Prepositions
+        
+        # Pronouns
+        r'\b(my)\b': 'mine',
+        r'\b(you|your|yours)\b': 'thou',
+        # Verbs
+        r'\b(are)\b': 'art',
         r'\b(should|shall)\b': 'shalt',
+        # Other
     }
     return replacer(value, word_dict)   
 
@@ -73,14 +122,24 @@ def rasta(value):
 
     """
     word_dict = {
-        r'\b(thou)\b': 'jah',
-        r'\b(depression)\b': 'uppression',
-        r'\b(depress)\b': 'uppress',
-        r'\b(yes|yeah)\b': 'yah',
-        r'\b(man)\b': 'mon',
+        # Adjectives
         r'\b(good|great)\b': 'irie',
+        # Adverbs
+        r'\b(yes|yeah)\b': 'yah',
+        # Expressions, phrases, and commands
+        
+        # Nouns
+        r'\b(depression)\b': 'uppression',
+        r'\b(god|God)\b': 'jah',
+        r'\b(man)\b': 'mon',
+        # Prepositions
+        
+        # Pronouns
         r'\b(me|my|I)\b': 'I and I',
-        r'\b(I)\b': 'I and I',
+        # Verbs
+        r'\b(depress)\b': 'uppress',
+        # Other
+        
     }
     return replacer(value, word_dict)
 
